22dce1e8d21613e0ea6a38d393fda3e6073132ef
PhilippKNMergener <philippknmergener@gmail.com>
2022-06-16 20:21:22 -0700

    completed landing page

---
+++ components/AboutMe/AboutMe.tsx
@@ -0,0 +1,9 @@
+import PageHeader from "../PageHeader/PageHeader";
+
+type Props = {};
+
+const AboutMe = (props: Props) => {
+  return <PageHeader number={1} title={"About Me"} />;
+};
+
+export default AboutMe;
